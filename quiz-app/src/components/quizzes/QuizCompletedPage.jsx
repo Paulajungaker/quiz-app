3430ad7309194642116d7fa8fd40fe4f769842e2
Paulajungaker <paula010514@hotmail.com>
2024-04-16 01:55:11 +0200

    Styling the completed page

--- quiz-app/src/components/quizzes/QuizCompletedPage.jsx
+++ quiz-app/src/components/quizzes/QuizCompletedPage.jsx
@@ -1,23 +1,34 @@
-import { Link, useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import CompletedImage from "../../../public/Celebrate.png";
 import "../../styling/QuizCompletedPage.css";
 
 const QuizCompletedPage = () => {
   const location = useLocation();
+  const navigate = useNavigate();
 
   const { state } = location;
   const score = state?.score;
   const totalQuestions = state?.totalQuestions;
 
+  const handleBackToHome = () => {
+    navigate("/");
+  };
+
   return (
     <div className="completedContainer">
       <h1 className="completedTitle">You have completed the Quiz</h1>
+      <img
+        src={CompletedImage}
+        alt="Icon with confetti"
+        className="completedImage"
+      />
       <p className="completedScore">
         Your score: {score} / {totalQuestions}
       </p>
       <p className="completedText">Thank you for taking the quiz</p>
-      <Link className="linkToHomepage" to="/">
-        Back to Home
-      </Link>
+      <button className="buttonToHomepage" onClick={handleBackToHome}>
+        Back to home
+      </button>
     </div>
   );
 };
