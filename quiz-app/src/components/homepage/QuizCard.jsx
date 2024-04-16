c5718afce4ca517280ce293eee8ca64b916d4ad9
Paulajungaker <paula010514@hotmail.com>
2024-04-16 12:00:36 +0200

    Changing som small issues with img paths and the link

--- quiz-app/src/components/homepage/QuizCard.jsx
+++ quiz-app/src/components/homepage/QuizCard.jsx
@@ -1,15 +1,11 @@
 import PropTypes from "prop-types";
 import "../../styling/QuizCard.css";
-import { Link } from "react-router-dom";
 
 const QuizCard = ({ quiz }) => {
   return (
     <div className="quizCardContainer">
       <h2 className="QuizCardTitle">{quiz.title}</h2>
       <img src={quiz.image} alt={quiz.title} className="quizCardImage" />
-      <div>
-        <Link to={`/quiz/${encodeURIComponent(quiz.title)}`}>Start Quiz</Link>
-      </div>
     </div>
   );
 };
