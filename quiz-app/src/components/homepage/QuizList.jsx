6082be92ac48201b915173c2c5723d6601a3cf6a
Paulajungaker <paula010514@hotmail.com>
2024-04-16 13:32:16 +0200

    Adding loading spinner

--- quiz-app/src/components/homepage/QuizList.jsx
+++ quiz-app/src/components/homepage/QuizList.jsx
@@ -1,18 +1,34 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { BsArrowRepeat } from "react-icons/bs";
 import QuizCard from "./QuizCard";
 import quizData from "../../quizData.json";
 
 const Quizlist = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <div>
       <h1 className="listTitle">Choose a Quiz</h1>
-      <div className="quizCards">
-        {quizData.map((quiz, index) => (
-          <Link key={index} to={`/quiz/${encodeURIComponent(quiz.title)}`}>
-            <QuizCard quiz={quiz} />
-          </Link>
-        ))}
-      </div>
+      {loading ? (
+        <div className="loadingContainer">
+          <BsArrowRepeat className="loadingIcon" />
+        </div>
+      ) : (
+        <div className="quizCards">
+          {quizData.map((quiz, index) => (
+            <Link key={index} to={`/quiz/${encodeURIComponent(quiz.title)}`}>
+              <QuizCard quiz={quiz} />
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
